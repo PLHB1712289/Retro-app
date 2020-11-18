aa3a2487f48774410d8cca3371739c230e0af6e3
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-11-18 14:58:02 +0700

    fix route with github page

--- src/App.js
+++ src/App.js
@@ -10,22 +10,23 @@ import SignIn from "./component/signIn";
 import SignUp from "./component/signUp";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import config from "./config";
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <Router>
         <Switch>
-          <Route path="/retro-app" exact>
+          <Route path={config.PUBLIC_URL} exact>
             <Redirect to="/" />
           </Route>
-          <Route path="/login">
+          <Route path={config.PUBLIC_URL + "/login"}>
             <SignIn />
           </Route>
-          <Route path="/register">
+          <Route path={config.PUBLIC_URL + "/register"}>
             <SignUp />
           </Route>
-          <Route path="/">
+          <Route path={config.PUBLIC_URL + "/"}>
             <HomePage />
           </Route>
           <Route>
--- src/config.js
+++ src/config.js
@@ -2,12 +2,14 @@ const dev = {
   API_URL: "http://localhost:3000",
   APP_NAME: "RETRO APP",
   APP_URL: "localhost",
+  PUBLIC_URL: "",
 };
 
 const prod = {
   API_URL: process.env.REACT_APP_API_URL,
   APP_NAME: process.env.REACT_APP_APP_NAME,
   APP_URL: process.env.REACT_APP_APP_URL,
+  PUBLIC_URL: process.env.REACT_APP_PUBLIC_URL,
 };
 
 const config = process.env.REACT_APP_STAGE === "production" ? prod : dev;
